913095c37ca32b8d661a81bc096dc6bb3927eeaa
sheilla da silva <sheilla.silva@queimadiaria.com.br>
2022-03-22 08:42:09 -0300

    feat: use react hook forms to step forms

--- src/FormStepOne.tsx
+++ src/FormStepOne.tsx
@@ -1,26 +1,85 @@
 import { useRouter } from "next/router";
 import { FC } from "react";
+import { Controller, FormProvider, useFormContext } from "react-hook-form";
 import { Box } from "theme-ui";
 import { Button } from "./components/Button";
 import { Card } from "./components/Card";
 import { Input } from "./components/Input";
 import { Title } from "./components/Title";
+import { useFormStepOne } from "./hooks/useFormStepOne";
+import { useTryCreateAccountStepOne } from "./hooks/useTryCreateAccountStepOne";
+import { UserAccountStepOne } from "./typings/userTyping";
 
-export const FormStepOne: FC = () => {
+export const FormStepOneContainer: FC = () => {
+    const form = useFormStepOne()
+
+    return <FormProvider {...form}><FormStepOne/></FormProvider>
+}
+
+const FormStepOne: FC = () => {
+    const {control, handleSubmit} = useFormContext<UserAccountStepOne>()
     const router = useRouter()
 
+    const {tryCreateAccountStepOne} = useTryCreateAccountStepOne()
+    
+    const submitStepOneForm = (form: UserAccountStepOne) => {
+        tryCreateAccountStepOne(form)
+        redirectCreateAccountStepTwo()
+    }
+    
     const redirectCreateAccountStepTwo = () => {
         const path = router.pathname
         router.push(path + '/step-two')
     }
 
     return <Card>
-    <Title type="secondary">Create Account</Title>
-    <Box>
-        <Input label="First Name" errorMessage="Error message"/>
-        <Input label="Last Name" errorMessage="Error message"/>
-        <Input label="Age Name" errorMessage="Error message"/>
-    </Box>
-    <Button sx={{width: '100%'}} onClick={redirectCreateAccountStepTwo}>Continuar</Button>
-</Card>
+        <form onSubmit={handleSubmit(submitStepOneForm)}>
+            <Title type="secondary">Create Account</Title>
+            <Box>
+                <Controller 
+                    name="firstName"
+                    control={control}
+                    render={({
+                        field: { onChange, onBlur, name },
+                        fieldState: { error },
+                    }) => ( 
+                    <Input label="First Name"  
+                        id={name} 
+                        onChange={event => onChange(event.target.value)} 
+                        onBlur={onBlur} 
+                        errorMessage={error?.message}/>
+                    )}/>
+                <Controller 
+                    name="lastName"
+                    control={control}
+                    render={({
+                        field: { onChange, onBlur, name },
+                        fieldState: { error },
+                    }) => ( 
+                    <Input 
+                        label="Last Name"
+                        id={name} 
+                        onChange={event => onChange(event.target.value)} 
+                        onBlur={onBlur} 
+                        errorMessage={error?.message}/>
+                    )}/>
+
+                <Controller 
+                    name="age"
+                    control={control}
+                    render={({
+                        field: { onChange, onBlur, name },
+                        fieldState: { error },
+                    }) => (
+                    <Input 
+                        label="Age" 
+                        id={name} 
+                        onChange={event => onChange(event.target.value)} 
+                        onBlur={onBlur} 
+                        errorMessage={error?.message}/>
+                    )}/>
+            </Box>
+            <Button sx={{width: '100%'}}>Continuar</Button>
+        </form>
+    </Card>
 }
--- src/FormStepTwo.tsx
+++ src/FormStepTwo.tsx
@@ -1,13 +1,31 @@
 import { useRouter } from "next/router";
 import { FC } from "react";
+import { Controller, FormProvider, useFormContext } from "react-hook-form";
 import { Box } from "theme-ui";
 import { Button } from "./components/Button";
 import { Card } from "./components/Card";
 import { Input } from "./components/Input";
 import { Title } from "./components/Title";
+import { useFormStepTwo } from "./hooks/useFormStepTwo";
+import { useTryCreateAccountStepTwo } from "./hooks/useTryCreateAccountStepTwo";
+import { UserAccountStepTwo } from "./typings/userTyping";
 
-export const FormStepTwo: FC = () => {
+export const FormStepTwoContainer: FC = () => {
+    const form = useFormStepTwo()
+
+    return <FormProvider {...form}><FormStepTwo/></FormProvider>
+}
+
+const FormStepTwo: FC = () => {
+    const {control, handleSubmit} = useFormContext<UserAccountStepTwo>()
     const router = useRouter()
+
+    const {tryCreateAccountStepTwo} = useTryCreateAccountStepTwo()
+    
+    const submitStepTwoForm = (form: UserAccountStepTwo) => {
+        tryCreateAccountStepTwo(form)
+        redirectSuccessPage()
+    }
 
     const redirectSuccessPage = () => {
         const createAccountPath = '/' + router.pathname.split('/')[1]        
@@ -15,12 +33,55 @@ export const FormStepTwo: FC = () => {
     }
 
     return <Card>
+        <form onSubmit={handleSubmit(submitStepTwoForm)}>
     <Title type="secondary">Create Account</Title>
     <Box>
-        <Input label="Email" errorMessage="Error message"/>
-        <Input label="Password" errorMessage="Error message"/>
-        <Input label="Confirm password" errorMessage="Error message"/>
+        <Controller 
+            name="email"
+            control={control}
+            render={({
+                field: { onChange, onBlur, name },
+                fieldState: { error },
+            }) => ( 
+            <Input 
+                label="Email"  
+                id={name} 
+                onChange={event => onChange(event.target.value)} 
+                onBlur={onBlur} 
+                errorMessage={error?.message}/>
+            )}/>
+        <Controller 
+            name="password"
+            control={control}
+            render={({
+                field: { onChange, onBlur, name },
+                fieldState: { error },
+            }) => ( 
+            <Input 
+                label="Password"
+                type='password'  
+                id={name} 
+                onChange={event => onChange(event.target.value)} 
+                onBlur={onBlur} 
+                errorMessage={error?.message}/>
+            )}/>
+        <Controller 
+            name="confirmPassword"
+            control={control}
+            render={({
+                field: { onChange, onBlur, name },
+                fieldState: { error },
+            }) => ( 
+            <Input 
+                label="Confirm password"  
+                id={name} 
+                type='password'  
+                onChange={event => onChange(event.target.value)} 
+                onBlur={onBlur} 
+                errorMessage={error?.message}/>
+            )}/>
     </Box>
-    <Button sx={{width: '100%'}} onClick={redirectSuccessPage}>Criar conta</Button>
+    <Button sx={{width: '100%'}}>Criar conta</Button>
+    </form>
 </Card>
 }
