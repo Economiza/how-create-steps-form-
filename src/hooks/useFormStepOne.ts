0f09f526760afab6e86ab3ec51ec2384c29c18cb
sheilla da silva <sheilla.silva@queimadiaria.com.br>
2022-03-22 08:41:07 -0300

    feat: add yup validation schema to use form steps

--- src/hooks/useFormStepOne.ts
+++ src/hooks/useFormStepOne.ts
@@ -1,7 +1,11 @@
 import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { stepOneAccountValidationSchema } from "../utils/accountValidationSchema";
+import { UserAccountStepOne } from "../typings/userTyping";
 
 export const useFormStepOne = () => {
     return useForm<UserAccountStepOne>({
         mode: 'onBlur',
+        resolver: yupResolver(stepOneAccountValidationSchema)
       });
 }
--- src/hooks/useFormStepTwo.ts
+++ src/hooks/useFormStepTwo.ts
@@ -1,7 +1,11 @@
+import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
+import { UserAccountStepTwo } from "../typings/userTyping";
+import { stepTwoAccountValidationSchema } from "../utils/accountValidationSchema";
 
-export const useFormStepOne = () => {
+export const useFormStepTwo = () => {
     return useForm<UserAccountStepTwo>({
         mode: 'onBlur',
+        resolver: yupResolver(stepTwoAccountValidationSchema)
       });
 }
