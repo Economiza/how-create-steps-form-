630b7d6ac38bd810d115ec5d84a05b5159d50a92
sheilla da silva <sheilla.silva@queimadiaria.com.br>
2022-03-22 10:20:01 -0300

    feat: create user account interface

---
+++ src/typings/userTypings.ts
@@ -0,0 +1,13 @@
+interface UserAccountFormStepOne {
+    firstName: string
+    lastName: string
+    age: number
+}
+
+interface UserAccountFormStepTwo {
+    email: string
+    password: string
+    confirmPassword: string
+}
+
+interface UserAccountForm extends UserAccountFormStepOne, UserAccountFormStepTwo {}
