846560b8f95868279f71d0fb5664b7fef7c13611
sheilla da silva <sheilla.silva@queimadiaria.com.br>
2022-03-22 10:35:16 -0300

    feat: create user account validation schema

---
+++ src/utils/userAccountValidationSchema.ts
@@ -0,0 +1,14 @@
+import * as yup from 'yup'
+
+export const userAccounStepOnetValidationSchema = yup.object({
+    firstName: yup.string().required('First name is required'),
+    lastName: yup.string().required('Last name is required'),
+    age: yup.number().required('Age is required').min(18, 'You must be 18 years old or above')
+})
+
+export const userAccounStepTwotValidationSchema = yup.object({
+    email: yup.string().required('Email is required').email('Invalid email'),
+    password: yup.string().required('Password is required').min(8, 'Password shold be 8 chars minimum'),
+    confirmPassword: yup.string().oneOf([yup.ref('password'), null], 'Password must be equals')   
+
+})
